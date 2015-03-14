2abf346baa2eae12392be71aa8d6b4b5c5664b71
vk <krthkv26@gmail.com>
2015-03-14 20:29:25 +0530

    add 1 line comment for generate_concat

--- generate_concatvideos.py
+++ generate_concatvideos.py
@@ -8,6 +8,7 @@ for arg in sys.argv:
 
 output_dir = all_args[1]
 
+# create a textfile that serves as input for ffmpeg to join all the individual videos
 with open("concatvideos",'w') as the_file:
     for videofile_name in sorted(glob.glob(output_dir+"*.mp4"), key=lambda name:int(name[len(output_dir):-4])):
         the_file.write("file '%s'\n" % videofile_name)
